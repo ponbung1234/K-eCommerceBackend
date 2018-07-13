1c587584ae4cc8d9936c6e5d5c97a48f0ac1371b
Thanapon Archint <thanapon.archint@gmail.com>
2018-07-13 11:33:26 +0700

    implement add cart function

--- demo/src/main/java/com/example/demo/AddCartController.java
+++ demo/src/main/java/com/example/demo/AddCartController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -18,14 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(allowedHeaders="*")
 public class AddCartController {
 	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody void getOrder(@RequestParam(value = "ecustomer_id", required = true)
-			String ecus_id,@RequestParam(value = "product_id", required = true) String product_id)
-			throws IOException, SQLException {
+	public @ResponseBody String cart(@RequestBody int cart) throws SQLException, IOException {
+	
+		int product_id = cart;
 		Database db = new Database();
 		Connection con = db.connect();
 		Statement stm = con.createStatement();
-		stm.executeUpdate("insert into cart(ecustomer_id,product_id,cart_amount) values("
-		+Integer.parseInt(ecus_id)+"," + Integer.parseInt(product_id)+","+"1)");
+		
+		ResultSet results;
+		results = stm.executeQuery("select product_id,cart_amount from cart where ecustomer_id = 1;");
+		results.beforeFirst();
+		int flag = 0;
+		int newCartAmount;
+		while (results.next()) {
+			if(product_id == results.getInt(1)) {
+				//update
+				newCartAmount = results.getInt(2);
+				newCartAmount++;
+				stm.executeUpdate("update cart set cart_amount = \"newCartAmount\" where product_id =" + product_id);
+				flag=1;
+			}
+		}
+		if(flag == 1) {
+			//insert
+			stm.executeUpdate("insert into cart(ecustomer_id,product_id,cart_amount)"
+					+ "values(1," + product_id + ", 1 )");
+		}
+
+	
+
 		db.close();
+
+		return "Action sucessful";
 	}
 }
